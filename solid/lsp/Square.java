19a9332a8b8e6db6dc674c1173c2165b653cc878
Chanon <gchax@outlook.com>
2022-10-19 19:32:20 +0700

    adding solid principles assignment

---
+++ solid/lsp/Square.java
@@ -0,0 +1,20 @@
+public class Square implements Shape {
+    private int side;
+
+    Square(int side) {
+        this.setSide(side);
+    }
+
+    public void setSide(int side) {
+        this.side = side;
+    }
+
+    public int getSide() {
+        return this.side;
+    }
+
+    @Override
+    public int getArea() {
+        return this.side * this.side;
+    }
+}
